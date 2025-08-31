906f2286a13ee75387178b34478a2d1b23950ff8
kryptycon <brandonethanzaini@gmail.com>
2025-08-31 20:49:53 +0700

    Changes

--- Booking.py
+++ Booking.py
@@ -38,6 +38,7 @@ class Start_Booking:
         self._appointment = appointment
         self._start_time = start_time
         self._end_time = end_time
+        self._status = True
 
     def Set_Date(self, set_date):
         self._date = set_date
@@ -53,61 +54,62 @@ class Start_Booking:
     Sets the Starting time of the booking appointment
     """
     
-    def Change_Appointment(self, New_Appointment_Type):
-        self._Appointment
+    def Change_Appointment(self, new_appointment_type):
+        self._Appointment = new_appointment_type
+
 
     """
     Changes the appointment type(the medical staff like nurse, GP or psychologist)
     """
 
     def Set_Staff(self, set_staff):
-        pass
+        self._staff_name = set_staff
 
     """
     It sets a new staff for the person
     """
 
     def Change_Staff(self, New_Staff):
-        pass
+        self._staff_name = New_Staff
     
     """
     Changes the Staff of the booking
     """
 
     def Get_Date(self):
-        pass
-
+        return self._date
+    
     """
     Get the date of the booking
     """
 
     def Get_Start_Time(self):
-        pass
+        return self._start_time
 
     """
     Get the Start Time of the appointment
     """
 
     def Get_End_Time(self):
-        pass
+        return self._end_time
     """
     Get the End Time of the appointment
     """
 
     def Get_Status(self):
-        pass
+        return self._status
     """
     Get the Status of the Booking (Booking is cancelled or not)
     """
 
     def Get_Staff(self):
-        pass
+        return self._staff_name
     """
     Get the Medical Staff (Nurse, GP, Psychologist)
     """
 
     def Get_Appointment(self):
-        pass
+        return self._appointment
 
     """
     Get the the Appointment for the medical staff
